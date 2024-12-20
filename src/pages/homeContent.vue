225040540b6414445e201a1505100ade1823da9f
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2024-12-20 23:24:41 +0200

    NoteList

--- src/pages/homeContent.vue
+++ src/pages/homeContent.vue
@@ -1,17 +1,29 @@
 <template>
   <NoteForm @onSubmit="handlerSubmit" />
+  <NoteList @onRemove="handleRemove" :items="notes" />
 </template>
 
 <script>
 import NoteForm from '@/components/Notes/NoteForm.vue'
+import NoteList from '@/components/Notes/NoteList.vue';
 export default {
   components: {
-    NoteForm
+    NoteForm,
+    NoteList
+  },
+  data() {
+    return {
+      notes: ['task 1', 'task 2', 'task 3'],
+      tags: ['home', 'work', 'travel']
+    }
   },
   methods: {
-    handlerSubmit(value) {
-      console.log(value);
+    handlerSubmit(note) {
+      this.notes.push(note)
     },
+    handleRemove(index) {
+      this.notes.splice(index, 1)
+    }
   }
 }
 </script>
