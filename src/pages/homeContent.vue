11aab475fa4463dcf1d186cfa12cd83f8ad44a33
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2024-12-20 19:11:17 +0200

    emit

--- src/pages/homeContent.vue
+++ src/pages/homeContent.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    hello home
-  </div>
+  <NoteForm @onSubmit="handlerSubmit" />
 </template>
+
+<script>
+import NoteForm from '@/components/Notes/NoteForm.vue'
+export default {
+  components: {
+    NoteForm
+  },
+  methods: {
+    handlerSubmit(value) {
+      console.log(value);
+    },
+  }
+}
+</script>
