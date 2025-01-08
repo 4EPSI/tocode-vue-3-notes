2749655daceaaa7b64b8d460b230f8ddb8f80435
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-01-08 16:07:35 +0200

    vuex

--- src/pages/VuexComponent.vue
+++ src/pages/VuexComponent.vue
@@ -1,8 +1,34 @@
 <template>
-    <div>vuex</div>
+    <ul style="margin-bottom: 25px;">
+      <li v-for="user in getUsers" :key="user.id">
+        <p>{{ user.id }}</p>
+        <p>{{ user.name }}</p>
+        <p>{{ user.admin ? 'admin' : 'user' }}</p>
+      </li>
+    </ul>
+
+    <input v-model="userId" type="number">
+    <p style="margin-top: 25px; margin-bottom: 25px; color: red;">{{ getUser }}</p>
+    <p style="color: blue;">number of users {{ getUsersLength }}</p>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userId: 2
+    }
+  },
+  computed: {
+    getUser() {
+      return this.$store.getters.getUsersById(this.userId) || 'not found'
+    },
+    getUsers() {
+      return this.$store.getters.getUsers
+    },
+    getUsersLength() {
+      return this.$store.getters.getUsersLength
+    }
+  }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,26 @@
 import { createStore } from "vuex";
 
-export const store = createStore({})
+export const store = createStore({
+  state: {
+    users: [
+      { id: 1, name: "John Doe", admin: true },
+      { id: 2, name: "Jane Doe", admin: false },
+      { id: 3, name: "Jim Doe", admin: false }
+    ]
+  },
+  getters: {
+    getAllUsers(state) {
+      return state.users
+    },
+    getUsers(state) {
+      return state.users.filter(user => !user.admin)
+    },
+    getUsersById: (state) => id => {
+      return state.users.find(user => user.id === id)
+    },
+    getUsersLength(state, getters) {
+      const suffix = 'number of users';
+      return `${suffix} ${getters.getAllUsers.length}`
+    }
+  }
+})
